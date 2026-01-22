9ae2332c6bdf19cc1deced712615706a41776361
oelvis105-ops <eoowusu17@users.noreply.github.com>
2026-01-22 20:16:43 +0000

    switch to user-session telethon

--- user_ping.py
+++ user_ping.py
@@ -1,10 +1,6 @@
 import os, asyncio
 from telethon import TelegramClient
-
-api_id   = int(os.environ["32454773"])
-api_hash = os.environ["f9789ae28af20dce11468e4f028dbe6b"]
-bot_id   = int(os.environ["8580432369"])
-my_id    = int(os.environ["1836471542"])
+...
 
 async def main():
     async with TelegramClient('session', api_id, api_hash) as client:
