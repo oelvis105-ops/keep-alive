d3ad583b4b90369bb03fdb39424a32be14612e94
MELLOW <oelvis105@gmail.com>
2026-01-22 10:59:10 +0000

    Add keep_alive.sh to manage Telegram bot
    This script sets environment variables for a Telegram bot and runs a Python script to keep the bot alive.

---
+++ keep_alive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export TG_API_ID=32454773
+export TG_API_HASH= f9789ae28af20dce11468e4f028dbe6b
+export BOT_ID=8580432369
+export MY_ID=1836471542
+
+curl -sL https://raw.githubusercontent.com/oelvis105-ops/keep-alive/refs/heads/main/user_ping.py -o ping.py
+python3 ping.py
+
+https://raw.githubusercontent.com/oelvis105-ops/keep-alive/refs/heads/main/user_ping.py
