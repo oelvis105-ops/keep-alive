2486ffd34366c645ee13c40272030066415982c5
MELLOW <oelvis105@gmail.com>
2026-01-22 19:36:17 +0000

    Change Telegram API and bot credentials
    Updated environment variable values for API and bot IDs.

--- user_ping.py
+++ user_ping.py
@@ -1,10 +1,10 @@
 import os, asyncio
 from telethon import TelegramClient
 
-api_id   = int(os.environ["TG_API_ID"])
-api_hash = os.environ["TG_API_HASH"]
-bot_id   = int(os.environ["BOT_ID"])
-my_id    = int(os.environ["MY_ID"])
+api_id   = int(os.environ["32454773"])
+api_hash = os.environ["f9789ae28af20dce11468e4f028dbe6b"]
+bot_id   = int(os.environ["8580432369"])
+my_id    = int(os.environ["1836471542"])
 
 async def main():
     async with TelegramClient('session', api_id, api_hash) as client:
